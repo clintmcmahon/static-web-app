53949f969c34c06c8ef3ce1b300c2d4bac851bab
Clint McMahon <clintmcmahon@Clints-MacBook-Air-2.local>
2024-10-22 15:15:06 -0500

    update

--- api/NonceInjector.cs
+++ api/NonceInjector.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Linq;
 
 namespace StaticWebApp.Test
 {
@@ -18,7 +20,20 @@ namespace StaticWebApp.Test
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+
+            // Get the directory where the function is running
+            string functionDirectory = Directory.GetCurrentDirectory();
+
+            // Get all directories and files in that directory
+            var directories = Directory.GetDirectories(functionDirectory);
+            var files = Directory.GetFiles(functionDirectory);
+
+            // Prepare a list of directories and files for display
+            var result = "Directories:\n" + string.Join("\n", directories.Select(d => Path.GetFileName(d))) +
+                         "\n\nFiles:\n" + string.Join("\n", files.Select(f => Path.GetFileName(f)));
+
+            // Return the result as part of the HTTP response
+            return new OkObjectResult(result);
         }
     }
 }
